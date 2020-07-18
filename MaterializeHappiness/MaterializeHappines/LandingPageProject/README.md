83c9af5c33b6f9a9a97fc18e3fb104a7226bba47
Milena <milenramiro@gmail.com>
2020-07-18 14:15:36 -0300

    Add 'MaterializeHappiness/MaterializeHappines/' from commit '82e88f75e76b8d0dfd5f8302999d899ca8a781e1'
    git-subtree-dir: MaterializeHappiness/MaterializeHappines
    git-subtree-mainline: 82e88f75e76b8d0dfd5f8302999d899ca8a781e1
    git-subtree-split: 82e88f75e76b8d0dfd5f8302999d899ca8a781e1

---
+++ MaterializeHappiness/MaterializeHappines/LandingPageProject/README.md
@@ -0,0 +1,3 @@
+# Landing Page :sparkles:
+## Sobre
+* Esta é a minha primeira landing page, baseada em um projeto que meu instrutor Guilherme Grillo criou, possui um design simples, responsivo e elegante.
---
+++ MaterializeHappiness/MaterializeHappines/ParallaxWebsite/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Descrição do meu site." />
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<meta name="keywords" content="palavras,separadas,por,virgula" />
+	<meta name="author" content="Milena">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body> 
+
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+
+				<div class="icons-social">
+					<a target="_black" href="ww.linkedin.com/in/milena-ramiro"><i class="fa fa-linkedin"></i></a>
+					<a target="_black" href="https://github.com/milena-ramiro"><i class="fa fa-github"></i></a>
+				</div><!--icons-social-->
+				<div class="clear"></div>
+
+				<div class="logo">
+					<p><span>{</span> Milena Ramiro <span>}</span></p>
+				</div><!--logo-->
+				<div class="clear"></div>
+
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--desktop-->
+				<nav class="mobile">
+					<i class="fa fa-bars"></i>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--mobile-->
+				<div class="clear"></div>
+			</header>
+
+			<div class="conteudo-header">
+				<h2>Bem-vindo(a) ao novo <span>Website!</span></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia iaculis ante et sodales. Mauris a volutpat dui, consectetur fermentum magna. Mauris sed lectus massa. Integer lobortis, tortor sit amet luctus luctus, enim ex mattis arcu, sed tempus enim diam ut mauris. Cras tristique laoreet sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras vel semper leo.
+				</p>
+				<p>Nullam aliquet nibh nibh, eget scelerisque nibh lacinia tempus. Quisque nec metus turpis. Nam efficitur, nisi eget faucibus hendrerit, lorem diam sagittis nibh, eget cursus sapien ligula id nisi. Donec pharetra euismod tristique. Morbi quis ex hendrerit purus cursus pretium. Nam maximus est vel justo vehicula, eu volutpat nibh efficitur. Sed vulputate viverra elit id accumsan. Curabitur eu leo elementum, interdum urna sit amet, semper risus.</p>
+				<a href="">Saiba mais</a>
+			</div><!--conteudo-header-->
+
+		</div><!--center-->
+
+	</section> 
+
+
+	<section class="plano">
+		<div class="chamada-planos">
+			<div class="center">
+				<h2>Confiram nossos planos</h2>
+			</div><!--center-->
+		</div><!--chamada-planos-->
+
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem Ipsum</h2>
+						<div class="pontos-plano">
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+						</div><!--pontos-plano-->
+						<div class="preco">
+							<h2>R$250,00</h2>
+						</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				</div><!--box-single-->
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem Ipsum</h2>
+						<div class="pontos-plano">
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+						</div><!--pontos-plano-->
+						<div class="preco">
+							<h2>R$250,00</h2>
+						</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				</div><!--box-single-->
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem Ipsum</h2>
+						<div class="pontos-plano">
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+								<p><i class="fa fa-check-square"></i>Lorem Ipsum dolor sit amet</p>
+						</div><!--pontos-plano-->
+
+						<div class="preco">
+							<h2>R$250,00</h2>
+						</div><!--preco-->
+
+					</div><!--box-planos-wraper-->
+				</div><!--box-single-->
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section>
+
+
+	<section class="paralax">
+		<div class="overlay-paralax"></div>
+		<div class="center">
+			<h2>Conheça nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, justo et rutrum mollis, arcu purus congue ex, sed efficitur augue nulla et risus. Curabitur sed ligula vel ante feugiat ullamcorper vitae et neque. Nunc convallis scelerisque vulputate. Nullam feugiat elit quam.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, justo et rutrum mollis, arcu purus congue ex, sed efficitur augue nulla et risus. Curabitur sed ligula vel ante feugiat ullamcorper vitae et neque. Nunc convallis scelerisque vulputate. Nullam feugiat elit quam,</p>
+		</div>
+
+	</section><!--paralax-->
+
+
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="border-efeito"></div>Entre em contato</h2>
+				<form>
+					<input type="text" placeholder="Insira seu nome..."/>
+					<input type="email" placeholder="Insira seu email..."/>
+					<textarea placeholder="Sua mensagem..."></textarea>
+					<input type="submit" value="Enviar" name="acao">
+				</form>
+			</div><!--w50-->
+
+			<div class="w50 time">
+				<h2><div class="border-efeito"></div>Conheça nosso time!</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div>
+						<div class="info-usuario">
+							<p style="font-weight: bold;">Milena Ramiro</p>
+							<p style="font-weight: italic;">CEO/Instrutora</p>
+						</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id tortor metus. Donec posuere sit amet felis et hendrerit. Aliquam pulvinar tincidunt purus, eget pulvinar urna semper sit amet.</p>
+					</div><!--descricao-usuario-box-->
+				</div><!--equipe-single-->
+
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div>
+						<div class="info-usuario">
+							<p style="font-weight: bold;">Milena Ramiro</p>
+							<p style="font-weight: italic;">CEO/Instrutora</p>
+						</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id tortor metus. Donec posuere sit amet felis et hendrerit. Aliquam pulvinar tincidunt purus, eget pulvinar urna semper sit amet.</p>
+					</div><!--descricao-usuario-box-->
+				</div><!--equipe-single-->
+
+				<div class="clear"></div>
+			</div><!--w50-->
+
+			<div class="clear"></div>
+		</div><!--center-->
+	</section>
+
+	<footer>
+		<p>Todos os direitos reservados.</p>
+	</footer>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+	<script>
+		$(function(){
+			$('nav.mobile i').click(function(){
+				var el = $(this).parent().find('ul');
+				if(el.is(':visible')==false){
+					el.fadeIn();
+				}
+				else{
+					el.fadeOut();
+				}
+			})
+		})
+	</script>
+
+</body>
+</html>
